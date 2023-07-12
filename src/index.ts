ec237051f7ce9c603055dcfbcd0f79d7ccb4af4b
Marco Ricupero <m.ricupero@avanade.com>
2023-07-12 14:42:29 +0200

    Multiplugin confing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { CollaborativeAbstractPlugin } from "./models/collaborative-plugin.class
 import { CollaborativeFields } from "./plugins/collaborative-fields";
 import { CollaborativePointer } from "./plugins/collaborative-pointer";
 
+export type PluginKeys = "fields" | "pointer";
+
 export class CollaborativeForm {
 	private socket: Socket;
 	private userData: IUserModel | undefined;
@@ -13,9 +15,14 @@ export class CollaborativeForm {
 		READY: "collaborative-form-ready",
 	};
 
+	private readonly PLUGIN_SETTINGS_MAP = {
+		fields: CollaborativeFields,
+		pointer: CollaborativePointer,
+	};
+
 	constructor(
 		host: string,
-		options: { plugins: { fields: boolean; pointer: boolean } } = {
+		private options: { plugins: { [key in PluginKeys]: boolean } } = {
 			plugins: { fields: true, pointer: true },
 		}
 	) {
@@ -46,14 +53,17 @@ export class CollaborativeForm {
 		if (!this.roomId || !this.userData?.userId) {
 			return;
 		}
-		[CollaborativeFields, CollaborativePointer].forEach((plugin) => {
-			this.plugins.push(
-				new plugin(this.socket, () => ({
-					...(this.userData || { userName: "", userId: "" }),
-					roomId: this.roomId,
-					path: window.location.pathname,
-				}))
-			);
-		});
+		Object.entries(this.PLUGIN_SETTINGS_MAP)
+			.filter(([key]) => this.options.plugins[key as PluginKeys] !== false)
+			.map(([key, pluginClass]) => pluginClass)
+			.forEach((plugin) => {
+				this.plugins.push(
+					new plugin(this.socket, () => ({
+						...(this.userData || { userName: "", userId: "" }),
+						roomId: this.roomId,
+						path: window.location.pathname,
+					}))
+				);
+			});
 	}
 }
